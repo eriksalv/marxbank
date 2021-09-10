96241062dd7f46a56e9d299d3f87091154ce661c
hermahs <hermahs@stud.ntnu.no>
2021-09-10 09:02:11 +0200

    fix(#4): rettet opp noen småe feil

--- core/src/main/java/it1901/User.java
+++ core/src/main/java/it1901/User.java
@@ -1,61 +1,59 @@
 package it1901;
 import java.util.ArrayList;
 
-private class User{
+class User {
 
-private String id;
-private String username;
-private String email;
-private String password;
-private ArrayList<Account> accounts = new ArrayList<>();
+    private String id;
+    private String username;
+    private String email;
+    private String password;
+    private ArrayList<IAccount> accounts = new ArrayList<IAccount>();
 
 
-public void setId( String newId){
-    this.id = newId;
-}
-public String getId(){
-    return id;
-}
-public void setUsername(String newUsername){
-    this.username = newUsername;
-}
-public String getUsername(){
-    return this.username;
-}
-public void setEmail(String newEmail){
-    this.email = newEmail;
-}
-public String getEmail(){
-    return this.email;
-}
-public void setPassword(String newPassword){
-    this.password = newPassword;
-}
-public String getPassword(){
-    return this.password;
-}
-public void setAccounts(ArrayList newAccountsList){
-    this.accounts = newAccountsList;
-}
-public ArrayList<Account> getAccounts(){
-    return this.accounts;
-}
+    public void setId(String newId) {
+        this.id = newId;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setUsername(String newUsername) {
+        this.username = newUsername;
+    }
+    public String getUsername() {
+        return this.username;
+    }
+    public void setEmail(String newEmail) {
+        this.email = newEmail;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    public void setPassword(String newPassword) {
+        this.password = newPassword;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+    public void setAccounts(ArrayList<IAccount> newAccountsList) {
+        this.accounts = newAccountsList;
+    }
+    public ArrayList<IAccount> getAccounts() {
+        return this.accounts;
+    }
 
-public void addAccount(Account newAccount){
-    if (!accounts.contains(newAccount)){
-    accounts.add(newAccount);
+    public void addAccount(IAccount newAccount) {
+        if (!accounts.contains(newAccount)) {
+            accounts.add(newAccount);
+        } else {
+            throw new IllegalArgumentException("This account is already added.");
+        }
     }
-    else {
-        throw new IllegalArgumentException("This account is already added.");
+    public void removeAccount(IAccount unwantedAccount) {
+        if (accounts.contains(unwantedAccount)) {
+            accounts.remove(unwantedAccount);
+        } else {
+            throw new IllegalArgumentException("This account does not exist in your accounts.");
+        }
     }
-}
-public void removeAccount(Account unwantedAccount){
-    if (accounts.contains(unwantedAccount)){
-        accounts.remove(unwantedAccount);
-    }
-    else {
-        throw new IllegalArgumentException("This account does not exist in your accounts.");
-    }
-}
 
 }
