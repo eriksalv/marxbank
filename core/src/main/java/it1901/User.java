a0a276cb5884df8fe7c7b14b22547d42fd346961
hermahs <hermahs@stud.ntnu.no>
2021-09-15 22:29:23 +0200

    feat(#26): startet på lokal lagring i User Klasse

--- core/src/main/java/it1901/User.java
+++ core/src/main/java/it1901/User.java
@@ -1,5 +1,14 @@
 package it1901;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
+import java.text.ParseException;
 import java.util.ArrayList;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 class User {
 
@@ -9,34 +18,51 @@ class User {
     private String password;
     private ArrayList<IAccount> accounts = new ArrayList<IAccount>();
 
+    public User(String id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {}
 
     public void setId(String newId) {
         this.id = newId;
     }
+
     public String getId() {
         return id;
     }
+
     public void setUsername(String newUsername) {
         this.username = newUsername;
     }
+
     public String getUsername() {
         return this.username;
     }
+
     public void setEmail(String newEmail) {
         this.email = newEmail;
     }
+
     public String getEmail() {
         return this.email;
     }
+
     public void setPassword(String newPassword) {
         this.password = newPassword;
     }
+
     public String getPassword() {
         return this.password;
     }
+
     public void setAccounts(ArrayList<IAccount> newAccountsList) {
         this.accounts = newAccountsList;
     }
+
     public ArrayList<IAccount> getAccounts() {
         return this.accounts;
     }
@@ -48,12 +74,142 @@ class User {
             throw new IllegalArgumentException("This account is already added.");
         }
     }
+
     public void removeAccount(IAccount unwantedAccount) {
         if (accounts.contains(unwantedAccount)) {
             accounts.remove(unwantedAccount);
         } else {
             throw new IllegalArgumentException("This account does not exist in your accounts.");
         }
+    }
+
+    /**
+     * saves a user to a jsonfile in data/users/
+     * The filename is structured like this: id.username.json
+     * @param user to be saved
+     * @throws IllegalArgumentException if user is null, or some of its fields are null
+     */
+    public static void saveUser(User user) {
+        // check if user is null
+        if(user == null) throw new IllegalArgumentException("User cannot be null");
+        // check if user fields are null
+        if(user.getId() == null || user.getUsername() == null || user.getEmail() == null || user.getPassword() == null) throw new IllegalArgumentException("User fields cannot be null");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        File userFile = new File(String.format("../data/users/%s.%s.json", user.getId(), user.getUsername()));
+        try {
+            objectMapper.writeValue(userFile, user);
+        } catch (Exception e) {
+            if(e instanceof FileNotFoundException) {
+                try {
+                    userFile = new File(String.format("data/users/%s.%s.json", user.getId(), user.getUsername()));
+                    objectMapper.writeValue(userFile, user);
+                } catch (Exception a) {
+                    System.out.println(String.format("Error: %s", a));
+                }
+            } else {
+                System.out.println(String.format("Error: %s", e));
+            }
+        }
+
+    }
+
+    public void saveUser() {
+        if(this.id == null || this.username == null || this.email == null || this.password == null) throw new IllegalArgumentException("User fields can not be null.");
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        File userFile = new File(String.format("../data/users/%s.%s.json", this.id, this.username));
+        try {
+            objectMapper.writeValue(userFile, this);
+        } catch (Exception e) {
+            if(e instanceof FileNotFoundException) {
+                try {
+                    userFile = new File(String.format("data/users/%s.%s.json", this.id, this.username));
+                    objectMapper.writeValue(userFile, this);
+                } catch (Exception a) {
+                    System.out.println(String.format("Error: %s", a));
+                }
+            } else {
+                System.out.println(String.format("Error: %s", e));
+            }
+        }
+    }
+
+    /**
+     * Reads a user from a given path to a jsonfile
+     * @param path to user
+     * @return the user at the given location or null if there is an error
+     * @throws FileNotFoundException if no userfile was found at path
+     * @throws IllegalArgumentException if path is not a valid path
+     */
+    public static User readUser(String path) throws FileNotFoundException {
+        // check if path is valid
+        if(!isValidPath(path)) throw new IllegalArgumentException("path is not valid");
+        // check if file exists
+        if(!new File(path).exists()) throw new FileNotFoundException("No File with that path exists.");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        User user;
+
+        File userFile = new File(path);
+
+        try {
+            user = objectMapper.readValue(userFile, User.class);
+        } catch (Exception e) {
+            user = null;
+        }
+
+        return user;
+    }
+
+    public void readUserToUser(String path) throws FileNotFoundException {
+        // check if path is valid
+        if(!isValidPath(path)) throw new IllegalArgumentException("path is not valid");
+        // check if file exists
+        if(!new File(path).exists()) throw new FileNotFoundException("No File with that path exists.");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        User user;
+        File userFile = new File(path);
+
+        try {
+            user = objectMapper.readValue(userFile, User.class);
+        } catch (Exception e) {
+            user = null;
+        }
+
+        if(user == null) throw new IllegalStateException("user is null");
+
+        this.setId(user.getId());
+        this.setUsername(user.getUsername());
+        this.setEmail(user.getEmail());
+        this.setPassword(user.getPassword());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("id:%s, username:%s", this.getId(), this.getUsername());
+    }
+
+    /**
+     * tests if a path is valid or not
+     * @param path to test if its valid
+     * @return true if path is valid, otherwise false
+     */
+    private static Boolean isValidPath(String path) {
+        try {
+            Paths.get(path);
+        } catch (InvalidPathException e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String... args) {
+        User u = new User("2", "testUsername", "test@email.com", "password");
+        u.saveUser();
     }
 
 }
--- core/src/test/java/it1901/UserTest.java
+++ core/src/test/java/it1901/UserTest.java
@@ -1,12 +1,23 @@
 package it1901;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 public class UserTest {
 
@@ -15,6 +26,14 @@ public class UserTest {
     @BeforeEach
     public void setup() {
         user = new User();
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        File f = new File("../data/users/99999.testUsername.json");
+        if(f.exists()) {
+          f.delete();  
+        }
     }
 
     @Test
@@ -36,6 +55,10 @@ public class UserTest {
         IAccount t = new SavingsAccount(5.0);
         user.addAccount(t);
         assertArrayEquals(new IAccount[] {t}, user.getAccounts().toArray());
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            user.addAccount(t);
+        });
     }
 
     @Test
@@ -53,5 +76,67 @@ public class UserTest {
         testAccounts.remove(0);
 
         assertArrayEquals(testAccounts.toArray(), user.getAccounts().toArray());
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            user.removeAccount(new SavingsAccount(8.0));
+        });
+    }
+
+    @Test
+    @DisplayName("test saveUser")
+    public void testSaveUser(@TempDir Path tempDir) throws Exception {
+        File file = new File("../data/users/99999.testUsername.json");
+
+        assertAll(
+            () -> assertThrows(IllegalArgumentException.class, () -> {
+                User.saveUser(null);
+            }),
+            () -> assertThrows(IllegalArgumentException.class, () -> {
+                User.saveUser(user);
+            })
+        );
+
+        user.setId("99999");
+        user.setUsername("testUsername");
+        user.setEmail("test@testemail.com");
+        user.setPassword("testpassword");
+
+        // test save user non static
+        user.saveUser();
+        assertTrue(Files.exists(file.toPath()), "File should exist");
+
+        // cleanup
+        cleanUp();
+
+        // test save user static
+        User.saveUser(user);
+        assertTrue(Files.exists(file.toPath()), "File should exist");
+    }
+
+    @Test
+    @DisplayName("test readUser")
+    public void testReadUser() throws FileNotFoundException {
+        user.setId("99999");
+        user.setUsername("testUsername");
+        user.setEmail("test@testemail.com");
+        user.setPassword("testpassword");
+
+        user.saveUser();
+        User t = User.readUser("../data/users/99999.testUsername.json");
+        assertAll(
+            () -> assertEquals(user.getId(), t.getId()),
+            () -> assertEquals(user.getUsername(), t.getUsername()),
+            () -> assertEquals(user.getEmail(), t.getEmail()),
+            () -> assertEquals(user.getPassword(), t.getPassword())
+        );
+        
+        User r = new User();
+        r.readUserToUser("../data/users/99999.testUsername.json");
+        assertAll(
+            () -> assertEquals(user.getId(), r.getId()),
+            () -> assertEquals(user.getUsername(), r.getUsername()),
+            () -> assertEquals(user.getEmail(), r.getEmail()),
+            () -> assertEquals(user.getPassword(), r.getPassword())
+        );
     }
 }
