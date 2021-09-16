9515b0d532d283ddcdcdc64a7584c8f105b942b2
Anna Østmo <annaost@stud.ntnu.no>
2021-09-16 15:18:45 +0200

    fxml(men ikke controller) til Login ferdig

--- marxbankfx/src/main/java/it1901/LogInController.java
+++ marxbankfx/src/main/java/it1901/LogInController.java
@@ -1,2 +1,16 @@
 package it1901;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LogInController {
+    @FXML private Label usernameLabel;
+    @FXML private Label passwordLabel;
+    @FXML private TextField typeUsername;
+    @FXML private TextField typePassword;
+    @FXML private Button registerButton;
+    
+
+}
