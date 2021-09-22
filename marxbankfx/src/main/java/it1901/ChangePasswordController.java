a555385daff50968f186bf9212375858ed8baa35
Anna Østmo <annaost@stud.ntnu.no>
2021-09-22 15:21:22 +0200

    fxml og controllere til profil og endre passord

---
+++ marxbankfx/src/main/java/it1901/ChangePasswordController.java
@@ -0,0 +1,33 @@
+package it1901;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ChangePasswordController {
+    
+    @FXML private Label changePasswordLabel;
+    @FXML private Label currentPasswordLabel;
+    @FXML private TextField currentPasswordField;
+    @FXML private Label newPasswordLabel;
+    @FXML private TextField newPasswordField;
+    @FXML private Label confirmNewPasswordLabel;
+    @FXML private TextField confirmNewPasswordField;
+    @FXML private Button saveButton;
+
+    private User user;
+
+    @FXML
+    private void intitialize(){
+    }
+    public void initData(User user) {
+        this.user = user;
+    }
+
+    public void handleSave(){
+        user.setPassword(newPasswordField.getText());
+        saveButton.setText("Oppdatert");
+    }
+
+}
--- marxbankfx/src/main/java/it1901/ProfileController.java
+++ marxbankfx/src/main/java/it1901/ProfileController.java
@@ -1,8 +1,14 @@
 package it1901;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 public class ProfileController {
     
@@ -11,4 +17,27 @@ public class ProfileController {
     @FXML private Label usernameLabel;
     @FXML private Label passwordLabel;
     @FXML private Button changePasswordButton;
+    @FXML private Pane newPane;
+
+    private User user;
+
+    @FXML
+    private void intitialize(){
+        nameLabel.setText(user.getEmail());
+        IDLabel.setText(user.getId());
+        usernameLabel.setText(user.getUsername());
+        passwordLabel.setText(user.getPassword());
+
+    }
+    private void handleChangePassword(ActionEvent e) throws IOException {       
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("ChangePassword.fxml"));
+        AnchorPane pane = loader.load();
+        ChangePasswordController controller = loader.getController();
+        controller.initData(user);
+
+        newPane.getChildren().setAll(pane);
+    }
+
+
 }
