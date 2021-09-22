db352e1a59a943a02af1e3df76dd786de7320b28
Anna Østmo <annaost@stud.ntnu.no>
2021-09-22 11:31:15 +0200

    fx ferdig, men ikke kontroller

---
+++ marxbankfx/src/main/java/it1901/ProfileController.java
@@ -0,0 +1,14 @@
+package it1901;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ProfileController {
+    
+    @FXML private Label nameLabel;
+    @FXML private Label IDLabel;
+    @FXML private Label usernameLabel;
+    @FXML private Label passwordLabel;
+    @FXML private Button changePasswordButton;
+}
