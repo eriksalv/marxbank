1bf51f8903167f47b2a28c06dca0a3eee0f3a145
eriksalv <eriksalv@stud.ntnu.no>
2021-09-23 11:27:47 +0200

    test(#46): MainControllerTest fungerer nå

--- marxbankfx/src/test/java/it1901/MainControllerTest.java
+++ marxbankfx/src/test/java/it1901/MainControllerTest.java
@@ -1,7 +1,34 @@
 package it1901;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class MainControllerTest extends ApplicationTest {
     
+    private MainController controller;
+
+    @Override
+    public void start(final Stage stage) throws Exception {
+        final FXMLLoader loader = new FXMLLoader(getClass().getResource("Main_test.fxml"));
+        final Parent root = loader.load();
+        this.controller = loader.getController();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+
+    @Test
+    public void testController_main() {
+        assertNotNull(controller);
+    }
 }
