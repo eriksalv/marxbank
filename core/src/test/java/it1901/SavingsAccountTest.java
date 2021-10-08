24b9ae34bde6f6cdffb621168f1c8ebfd0c90aea
hermahs <hermahs@stud.ntnu.no>
2021-10-08 15:07:23 +0200

    test(#34): lagt til savingsaccountTest igjen

--- core/src/test/java/it1901/SavingsAccountTest.java
+++ core/src/test/java/it1901/SavingsAccountTest.java
@@ -1,5 +1,8 @@
 package it1901;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,7 +16,7 @@ import org.junit.jupiter.api.io.TempDir;
 public class SavingsAccountTest {
     
     @TempDir
-    private static Path tempDir;
+    static Path tempDir;
     private DataManager dm;
     private User user;
 
@@ -30,9 +33,42 @@ public class SavingsAccountTest {
 
     @Test
     @DisplayName("test of first constructor")
-    public void testFirstConstructor() {
-        SavingsAccount a = new SavingsAccount("id", user, 3, dm);
-        
+    public void testConstructors() {
+        SavingsAccount normalConstrutor = new SavingsAccount("1", user, 3, dm);
+        assertAll(
+            () -> assertEquals("1", normalConstrutor.getId()),
+            () -> assertEquals(user, normalConstrutor.getUser()),
+            () ->  assertEquals(3.0, normalConstrutor.getInterestRate())
+        );
+
+        SavingsAccount normalSetRateConstructor = new SavingsAccount("2", user, 5.0, dm);
+        assertAll(
+            () -> assertEquals("2", normalSetRateConstructor.getId()),
+            () -> assertEquals(user, normalSetRateConstructor.getUser()),
+            () -> assertEquals(5.0, normalSetRateConstructor.getInterestRate())
+        );
+
+        SavingsAccount noIdNameConstructor = new SavingsAccount(user, dm, "name");
+        assertAll(
+            () -> assertEquals(user, noIdNameConstructor.getUser()),
+            () -> assertEquals("name", noIdNameConstructor.getName())  
+        );
+
+        SavingsAccount longConstructor = new SavingsAccount("3", user, 5.0, dm, "longConstructor", 5);
+        assertAll(
+            () -> assertEquals("3", longConstructor.getId()),
+            () -> assertEquals(user, longConstructor.getUser()),
+            () -> assertEquals(5.0, longConstructor.getInterestRate()),
+            () -> assertEquals("longConstructor", longConstructor.getName()),
+            () -> assertEquals(5, longConstructor.getAccountNumber())
+        );   
+    }
+
+    @Test
+    @DisplayName("test get account type")
+    public void testGetAccountType() {
+        SavingsAccount a = new SavingsAccount(user, dm, "name");
+        assertEquals("Sparekonto", a.getAccountType());
     }
 
     
