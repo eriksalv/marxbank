db1e68b85a39a5c7dd6cd35a49dd5e220b75ad22
unknown <simeonc@stud.ntnu.no>
2021-10-10 10:59:12 +0200

    (#58): Forenklet logikken til sparekalkulatoren

--- core/src/main/java/it1901/SavingsCalc.java
+++ core/src/main/java/it1901/SavingsCalc.java
@@ -3,11 +3,6 @@ package it1901;
 public class SavingsCalc {
     
     public static long calculation(int monthlyAmount, int lumpAmount, double interestRate, int period) {
-        if (monthlyAmount == 0) {
-            double sum = lumpAmount * Math.pow(1 + (interestRate / 100), period);
-            return Math.round(sum);
-        }
-
         double lumpAmountSum = lumpAmount;
         double monthlyAmountSum = 0;
 
@@ -17,9 +12,14 @@ public class SavingsCalc {
             for (int j = 1; j <= 12; j++) {
                 monthlyAmountSum += monthlyAmount * (1 + (j * interestRate / 12));
             }
-            
         }
+        
         return Math.round(lumpAmountSum + monthlyAmountSum);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(SavingsCalc.calculation(0, 1000, 0.10, 5));
     }
 
 }
