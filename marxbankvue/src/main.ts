e78d49dbacd54b96047e52af9b8c1a154abe2122
Erik Salvesen <eriksalv@stud.ntnu.no>
2021-10-19 16:17:58 +0200

    feat(#106): lagt til vuex og testet det ut
    Co-authored-by: @simeonc

--- marxbankvue/src/main.ts
+++ marxbankvue/src/main.ts
@@ -1,4 +1,28 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import HelloWorldComponent from './components/HelloWorld.vue'
+import { createStore } from 'vuex'
 
-createApp(App).mount('#app')
+// Create a new store instance.
+const store = createStore({
+  state: {
+      count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+const app = createApp(App)
+const vm = app.mount('#app')
+
+// Install the store instance as a plugin
+app.use(store)
+
+store.commit('increment')
+store.commit('increment')
+store.commit('increment')
+
+console.log(store.state.count) // -> 3
+
