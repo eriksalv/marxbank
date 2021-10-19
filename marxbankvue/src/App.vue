a75edd49cd12bbe6c65057d0e565280731169e32
Erik Salvesen <eriksalv@stud.ntnu.no>
2021-10-19 19:03:44 +0200

    feat(#106): satt opp routing
    Co-authored-by: @simeonc

--- marxbankvue/src/App.vue
+++ marxbankvue/src/App.vue
@@ -1,4 +1,35 @@
 <template>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      <router-view></router-view>
+    </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+<!--
+<template>
   <div class="container">
     <Header title="Marx Bank"></Header>
     <Accounts></Accounts>
@@ -29,3 +60,4 @@ export default class App extends Vue {}
   margin-top: 60px;
 }
 </style>
+-->
