d5693f52f017fadf5e2b801afc8c9ff4e667e101
Herman Håberg Seternes <hermahs@stud.ntnu.no>
2021-10-23 18:32:42 +0200

    feat(#125): laget ferdig en call wrapper som returnerer et promise

---
+++ marxbankvue/src/service/restService.ts
@@ -0,0 +1,61 @@
+import axios, {AxiosPromise, AxiosInstance, AxiosRequestHeaders} from 'axios';
+
+export function promiseWrapper<T>(axios: AxiosPromise<T>): Promise<T> {
+    return new Promise((resolve, reject) => {
+        axios
+        .then(d => resolve(d.data))
+        .catch(async error => {
+            if (error.response) {
+                console.error(error.response.status);
+                reject(error.response.data.message);
+            } else {
+                reject(error.message);
+            }
+        })
+    })
+}
+
+export const getValidate = (status: number) => status === 200;
+
+// TODO skrive om slik at den er vår egen
+
+export class restService {
+
+    private readonly handler: AxiosInstance;
+    // lage en axios objekt
+    public constructor() {
+        this.handler = axios.create();
+    }
+
+    public setUrl(url: string) {
+        this.handler.defaults.baseURL = url;
+    }
+
+    public setToken(token: string | null) {
+        if (token === null) delete this.handler.defaults.headers.common["Authorization"];
+        else this.handler.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    }
+
+    // hente GETS
+    public get<T> (url: string, config?: {params?: AxiosRequestHeaders, headers?: AxiosRequestHeaders}): Promise<T> {
+        return promiseWrapper(this.handler.get(url, {validateStatus: code => code === 200, ...config}));
+    }
+    // POSTS
+    public post (url: string, data?: object): Promise<void> {
+        return promiseWrapper(this.handler.post(url, data, {validateStatus: code => (code === 200 || code === 201 || code === 204)}));
+    }
+
+    public postData<T> (url: string, data?: object): Promise<T> {
+        return promiseWrapper(this.handler.post(url, data, {validateStatus: code => code === 200}));
+    }
+
+    // PUTS
+    public put (url: string, data?: object): Promise<void> {
+        return promiseWrapper(this.handler.put(url, data, {validateStatus: code => (code === 200 || code === 204)}));
+    }
+
+    // DELETES
+    public delete (url: string): Promise<void> {
+        return promiseWrapper(this.handler.delete(url, {validateStatus: code => (code === 200 || code === 202 || code === 204)}));
+    }
+}
