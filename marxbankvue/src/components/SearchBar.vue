8b49ea540d26f60b195743aab796624cce3a2d6f
Erik Salvesen <eriksalv@stud.ntnu.no>
2021-10-25 00:04:17 +0200

    feat(#123): refaktorisert SearchBar
    Er mer gjenbrukelig nå

---
+++ marxbankvue/src/components/SearchBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div className="searchInputs">
+        <input @input="onInput" ref="inputField" type="text" :placeholder="placeholder" className="input"/>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "SearchBar",
+    methods: {
+        onInput(event) {
+            this.$emit("termChanged", event.target.value);
+        },
+        clearInput() {
+            this.$refs.inputField.value = "";
+        }
+    },
+    props: {
+        placeholder: String
+    }
+}
+</script>
+
+<style>
+
+</style>
--- marxbankvue/src/views/CreateTransaction.vue
+++ marxbankvue/src/views/CreateTransaction.vue
@@ -1,24 +1,50 @@
 <template>
 <main class="w-1/4">
-    <div id="reciever" class="relative w-full rounded-sm inline-block">
-        <h1>Velg en konto</h1>
-        <SearchBar @accountSelected="onAccountSelected"/>
+    <h1>Velg en konto</h1>
+    <div className="search">
+        <SearchBar ref="inputAccount" @termChanged="onTermChanged" :placeholder="recieverPlaceholder"/>
+        <div className="dataResult">
+            <div v-for="account in filterAccountsByName(searchTerm)" :key="account.id"
+                className="dataItem" @click="selectAccount(account)">
+                <p class="font-bold">{{ account.name }}</p>
+                <p>{{ account.accNumber }}</p>
+            </div>
+        </div>
     </div>
 </main>
 </template>
 
 <script>
-import SearchBar from '@/components/createTransaction/SearchBar.vue'
+import SearchBar from '@/components/SearchBar.vue';
+import { mapGetters } from 'vuex';
+import Account from '@/store/modules/accounts.ts';
 
 export default {
     name: 'CreateTransaction',
     components: {
         SearchBar,
     },
+    computed: mapGetters(["filterAccountsByName"]),
     methods: {
-        onAccountSelected(account) {
+        selectAccount(account) {
             //TODO: vis info om kontoen (kontonummer)
+            this.selectedRecieverAccount = account;
+            this.recieverPlaceholder = account.name;
+            this.searchTerm = "";
+            this.$refs.inputAccount.clearInput();
             console.log(account);
+        },
+        onTermChanged(searchTerm) {
+            this.searchTerm = searchTerm;
+            console.log(this.searchTerm);
+        }
+
+    },
+    data() {
+        return {
+            selectedRecieverAccount: Account,
+            recieverPlaceholder: "Søk...",
+            searchTerm: "",
         }
     }
 }
