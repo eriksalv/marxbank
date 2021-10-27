4f69fe5a09822ca1f4ff50ce85f4eed1ade23a06
Erik Salvesen <eriksalv@stud.ntnu.no>
2021-10-28 00:34:15 +0200

    feat(#130): laget et skjema for å lage ny konto
    Er definert som AccountForm.vue

--- marxbankvue/src/views/CreateTransaction.vue
+++ marxbankvue/src/views/CreateTransaction.vue
@@ -12,9 +12,7 @@
         @accountSelected="onRecieverAccountSelected"/>
     <h1>Kroner</h1>
     <input type="number" ref="amount" className="input">
-    <button @click="commitTransaction" class="bg-green-500 hover:bg-green-400 
-        text-white font-bold py-2 px-4 border-b-4 border-green-700 
-        hover:border-green-500 rounded w-full mt-6">
+    <button @click="commitTransaction" class="button">
             Utfør transaksjon
     </button>
 </main>
