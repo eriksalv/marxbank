79a1fd91b9782d4a80ede43f14eb39c079bbd28e
Anna Østmo <annaost@stud.ntnu.no>
2021-10-28 10:59:50 +0200

    feat(#115): fikser i rar feil

--- backend/src/main/java/marxbank/endpoint/TransactionController.java
+++ backend/src/main/java/marxbank/endpoint/TransactionController.java
@@ -16,7 +16,7 @@ import marxbank.repository.TransactionRepository;
 
 
 @RestController
-@RequestMapping("{user}/{transactions}")
+@RequestMapping("/transactions")
 public class TransactionController {
 
     private final TransactionRepository transactionRepository;
--- backend/src/main/java/marxbank/repository/TransactionRepository.java
+++ backend/src/main/java/marxbank/repository/TransactionRepository.java
@@ -3,11 +3,11 @@ package marxbank.repository;
 import java.util.Optional;
 
 import marxbank.model.Transaction;
-import marxbank.model.User;
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface TransactionRepository extends CrudRepository<Transaction, Long>{
     
-    Optional<Transaction> findUserTransactions(User User);
+    Optional<Transaction> findById(Long id);
+    Optional<Transaction> findByFrom_Id(Long id);
+    Optional<Transaction> findByReciever_Id(Long id);
 }
