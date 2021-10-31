b8745da39ee9f032bc11f39741d664276af83b66
Herman Håberg Seternes <hermahs@stud.ntnu.no>
2021-10-31 22:05:18 +0100

    fix(#116): lagt til fillertest slik at backend pipeline fungerer

---
+++ backend/src/test/java/marxbank/FillerTest.java
@@ -0,0 +1,14 @@
+package marxbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FillerTest {
+    
+    @Test
+    public void fillerTest() {
+        System.out.println("yeet");
+    }
+
+}
