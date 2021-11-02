47fc92cb9a5511c9dd55823aaeae6a23ca1ed63e
Erik Salvesen <eriksalv@stud.ntnu.no>
2021-11-02 19:52:09 +0100

    feat(#131): glemte å eksportere ting

--- marxbankvue/src/store/modules/auth/index.ts
+++ marxbankvue/src/store/modules/auth/index.ts
@@ -1,6 +1,9 @@
 import { AuthState } from "./types";
 import { RootState } from "@/store/types";
 import { Module } from "vuex";
+import { getters } from "./getters";
+import { mutations } from "./mutations";
+import { actions } from "./actions";
 
 const state: AuthState = {
   status: "",
@@ -10,4 +13,7 @@ const state: AuthState = {
 
 export const auth: Module<AuthState, RootState> = {
   state,
+  getters,
+  mutations,
+  actions,
 };
