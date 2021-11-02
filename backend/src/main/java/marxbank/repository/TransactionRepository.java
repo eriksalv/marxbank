22bd227d83a8811c5ed4b2c4b217b85043615e43
Erik Salvesen <eriksalv@stud.ntnu.no>
2021-11-02 22:41:04 +0100

    feat(#131): merge fix

--- backend/src/main/java/marxbank/repository/TransactionRepository.java
+++ backend/src/main/java/marxbank/repository/TransactionRepository.java
@@ -11,8 +11,4 @@ public interface TransactionRepository extends CrudRepository<Transaction, Long>
     
     Optional<List<Transaction>> findTransactionByFrom_id(Long accountId);
     Optional<List<Transaction>> findTransactionByReciever_id(Long account);
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
 }
