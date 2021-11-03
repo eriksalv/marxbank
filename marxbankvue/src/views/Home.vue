a00d0f48e88037f90d8ea18b542805ff8071b062
unknown <simeonc@stud.ntnu.no>
2021-11-03 14:35:03 +0100

    ci(#120): designet ferdig hovedmenyen

--- marxbankvue/src/views/Home.vue
+++ marxbankvue/src/views/Home.vue
@@ -54,7 +54,7 @@
           <button  class="bg-green-500 hover:bg-green-400 
             text-white font-bold py-2 px-4 border-b-4 border-green-700 
             hover:border-green-500 rounded  mt-6">
-              Prøv sparekalkulatoren!
+               Prøv sparekalkulatoren!
           </button>
         </router-link>
       </div>
