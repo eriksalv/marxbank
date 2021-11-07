c61491984028243354214529dc5bf0637a8da12f
Herman Håberg Seternes <hermahs@stud.ntnu.no>
2021-11-07 13:21:00 +0100

    test(#149): laget ferdig tester for account

--- core/src/main/java/marxbank/API/AccountRequest.java
+++ core/src/main/java/marxbank/API/AccountRequest.java
@@ -1,5 +1,10 @@
 package marxbank.API;
 
+import marxbank.model.Account;
+import marxbank.model.CheckingAccount;
+import marxbank.model.CreditAccount;
+import marxbank.model.MarxAccount;
+import marxbank.model.SavingsAccount;
 import marxbank.util.AccountType;
 
 public class AccountRequest {
@@ -27,5 +32,33 @@ public class AccountRequest {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Account buildAccount() {
+        Account a = null;
+
+        if (type == AccountType.SAVING) {
+            a = new SavingsAccount();
+            a.setName(name);
+            a.setInterestRate(3.0);
+            a.setType(type);
+        } else if (type == AccountType.CHECKING) {
+            a = new CheckingAccount();
+            a.setName(name);
+            a.setInterestRate(0.5);
+            a.setType(type);
+        } else if (type == AccountType.CREDIT) {
+            a = new CreditAccount();
+            a.setName(name);
+            a.setInterestRate(0);
+            a.setType(type);
+        } else if (type == AccountType.MARX) {
+            a = new MarxAccount();
+            a.setName(name);
+            a.setInterestRate(0.01);
+            a.setType(type);
+        }
+
+        return a;
     }
 }
