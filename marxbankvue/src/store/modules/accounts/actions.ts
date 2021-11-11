b995a8a544310f55e50947abc202852cc8a818b3
Erik Salvesen <eriksalv@stud.ntnu.no>
2021-11-11 08:44:12 +0100

    feat(#160): ny action for å fetche én konto

--- marxbankvue/src/store/modules/accounts/actions.ts
+++ marxbankvue/src/store/modules/accounts/actions.ts
@@ -1,15 +1,13 @@
 import { RootState } from "@/store/types";
 import { Account, AccountRequest, DepositWithdrawRequest } from "@/types/types";
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { ActionTree } from "vuex";
-import { auth } from "../auth";
 import { AccountState } from "./types";
 
 const BASE_URL = "/accounts";
 
 export const actions: ActionTree<AccountState, RootState> = {
   async fetchAccounts({ commit, rootGetters }) {
-    //TODO: få tak i token fra auth modul når den er implementert
     commit("setAccountStatus", "loading");
     await axios
       .get(`${BASE_URL}/myAccounts`, rootGetters.getToken)
@@ -25,8 +23,8 @@ export const actions: ActionTree<AccountState, RootState> = {
             accNumber: element.accountNumber,
             balance: element.balance,
             type: element.type,
-            interest: element.interestRate
-          }
+            interest: element.interestRate,
+          };
           accounts = [...accounts, account];
         });
         commit("setAccounts", accounts);
@@ -34,10 +32,37 @@ export const actions: ActionTree<AccountState, RootState> = {
       })
       .catch((err) => {
         commit("setAccountStatus", "error");
-      })
+      });
   },
 
-  async createAccount({commit, rootGetters}, request: AccountRequest) {
+  async fetchById({ commit, rootGetters }, id: number) {
+    commit("setAccountStatus", "loading");
+    await axios
+      .get(`${BASE_URL}/${id}`)
+      .then((response) => {
+        const account: Account = {
+          id: response.data.id,
+          userId: response.data.userId,
+          name: response.data.name,
+          accNumber: response.data.accountNumber,
+          balance: response.data.balance,
+          type: response.data.type,
+          interest: response.data.interestRate,
+        };
+        const allAccounts: Array<Account> = rootGetters.allAccounts;
+        if (allAccounts.map((a: Account) => a.id).includes(account.id)) {
+          commit("updateAccount", account);
+        } else {
+          commit("addAccount", account);
+        }
+        commit("setAccountStatus", "success");
+      })
+      .catch((err) => {
+        commit("setAccountStatus", "error", err);
+      });
+  },
+
+  async createAccount({ commit, rootGetters }, request: AccountRequest) {
     commit("setAccountStatus", "loading");
     await axios
       .post(`${BASE_URL}/createAccount`, request, {
@@ -53,17 +78,17 @@ export const actions: ActionTree<AccountState, RootState> = {
           accNumber: response.data.accountNumber,
           balance: response.data.balance,
           type: response.data.type,
-          interest: response.data.interestRate
-        }
+          interest: response.data.interestRate,
+        };
         commit("addAccount", account);
         commit("setAccountStatus", "success");
       })
       .catch((err) => {
         commit("setAccountStatus", "error");
-      })
+      });
   },
 
-  async deposit({commit, rootGetters}, request: DepositWithdrawRequest) {
+  async deposit({ commit, rootGetters }, request: DepositWithdrawRequest) {
     commit("setAccountStatus", "loading");
     await axios
       .post(`${BASE_URL}/deposit`, request, {
@@ -79,17 +104,17 @@ export const actions: ActionTree<AccountState, RootState> = {
           accNumber: response.data.accountNumber,
           balance: response.data.balance,
           type: response.data.type,
-          interest: response.data.interestRate
-        }
+          interest: response.data.interestRate,
+        };
         commit("updateAccount", account);
         commit("setAccountStatus", "success");
       })
       .catch((err) => {
         commit("setAccountStatus", "error");
-      })
+      });
   },
 
-  async withdraw({commit, rootGetters}, request: DepositWithdrawRequest) {
+  async withdraw({ commit, rootGetters }, request: DepositWithdrawRequest) {
     commit("setAccountStatus", "loading");
     await axios
       .post(`${BASE_URL}/withdraw`, request, {
@@ -105,13 +130,13 @@ export const actions: ActionTree<AccountState, RootState> = {
           accNumber: response.data.accountNumber,
           balance: response.data.balance,
           type: response.data.type,
-          interest: response.data.interestRate
-        }
+          interest: response.data.interestRate,
+        };
         commit("updateAccount", account);
         commit("setAccountStatus", "success");
       })
       .catch((err) => {
         commit("setAccountStatus", "error");
-      })
-  }
+      });
+  },
 };
